5bd8ed43c77c521039c1715aa36de2abd7d787f8
sumanta <sumantajashh@gmail.com>
2025-07-18 21:46:45 +0530

    Basic Structure

---
+++ backend/src/main/java/com/sumanta/MainGenie/MainGenieApplication.java
@@ -0,0 +1,13 @@
+package com.sumanta.MainGenie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainGenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainGenieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sumanta/MainGenie/MainGenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumanta.MainGenie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainGenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
