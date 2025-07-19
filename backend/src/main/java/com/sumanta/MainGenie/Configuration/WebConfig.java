709b11c98589f31635aef938db8d7d8d76651713
sumanta <sumantajashh@gmail.com>
2025-07-19 16:53:53 +0530

    web config file & popup frontend modification

---
+++ backend/src/main/java/com/sumanta/MainGenie/Configuration/WebConfig.java
@@ -0,0 +1,22 @@
+package com.sumanta.MainGenie.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}
